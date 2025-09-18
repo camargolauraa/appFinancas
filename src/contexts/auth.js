782e948719306dc73862038226ddc9dbac42a257
Laura Camargo <camargolauraa@gmail.com>
2025-09-18 09:02:59 -0300

    feat: loading para SignUp

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -5,27 +5,30 @@ import api from "../services/api";
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  const [user, setUser] = useState({
-    nome: "Laura",
-  });
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
   async function signUp(email, password, nome) {
+    setLoading(true);
+
     try {
       const response = await api.post("/users", {
         name: nome,
         password: password,
         email: email,
       });
+      setLoading(false);
       navigation.goBack();
     } catch (error) {
       console.error("Erro ao cadastrar:", error);
+      setLoading(false);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ user, signUp }}>
+    <AuthContext.Provider value={{ user, signUp, loading }}>
       {children}
     </AuthContext.Provider>
   );
