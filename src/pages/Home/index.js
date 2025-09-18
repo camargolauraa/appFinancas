bc9fa5345027d3c473ad536b5747985983052c34
Laura Camargo <camargolauraa@gmail.com>
2025-09-18 20:36:36 -0300

    feat: header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,17 +2,14 @@ import React, { useContext } from "react";
 import { View, Text, Button } from "react-native";
 
 import { AuthContext } from "../../contexts/auth";
-import { Container, SubmitButton, SubmitText } from "../SignIn/styles";
+
+import Header from "../../components/header";
+import { Background } from "./styles";
 
 export default function Home() {
-  const { signOut, user } = useContext(AuthContext);
   return (
-    <Container>
-      <Text>Home Page</Text>
-      <Text>Olá, {user.name}!</Text>
-      <SubmitButton title="Logout" onPress={signOut}>
-        <SubmitText>Logout</SubmitText>
-      </SubmitButton>
-    </Container>
+    <Background>
+      <Header title="Minhas Movimentações" />
+    </Background>
   );
 }
