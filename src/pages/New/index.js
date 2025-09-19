0af9ca2650f51a45f551add89b61a5ed8b216de3
Laura Camargo <camargolauraa@gmail.com>
2025-09-18 23:18:18 -0300

    feat: registrando movimentações na conta

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,14 +1,58 @@
 import React, { useState } from "react";
 import { Background, Input, SubmitButton, SubmitText } from "./styles";
-import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { View, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 
 import Header from "../../components/Header";
 import RegisterTypes from "../../components/RegistretTypes";
 
+import api from "../../services/api";
+import { format } from "date-fns";
+import { useNavigation } from "@react-navigation/native";
+
 export default function New() {
+  const navigation = useNavigation();
+
   const [labelInput, setLabelInput] = useState("");
   const [valueInput, setValueInput] = useState("");
   const [type, setType] = useState("receita");
+
+  function handleSubmit() {
+    Keyboard.dismiss();
+    if (isNaN(parseFloat(valueInput)) || type === null) {
+      alert("Preencha todos os campos!");
+      return;
+    }
+    Alert.alert(
+      "Confirmando dados",
+      `Tipo: ${type} - Valor: ${parseFloat(valueInput).toFixed(2)}`,
+      [
+        {
+          text: "Cancelar",
+          style: "cancel",
+        },
+        {
+          text: "Confirmar",
+          onPress: () => {
+            handleAdd();
+          },
+        },
+      ]
+    );
+  }
+
+  async function handleAdd() {
+    Keyboard.dismiss();
+    await api.post("/receive", {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), "dd/MM/yyyy"),
+    });
+
+    setLabelInput("");
+    setValueInput("");
+    navigation.navigate("Home"); // Não faz sentido voltar para a home. Se houver mais de um registro, o usuário terá que clicar em "Novo" novamente.
+  }
 
   return (
     <TouchableWithoutFeedback
@@ -36,7 +80,11 @@ export default function New() {
             sendTypeChanged={(item) => setType(item)}
           />
 
-          <SubmitButton>
+          <SubmitButton
+            onPress={() => {
+              handleSubmit();
+            }}
+          >
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
         </View>
